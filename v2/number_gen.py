53afaf5e0cef5c7fa9271e49e47b2834a406693a
Milena Vujicic <vujicic_milena@uns.ac.rs>
2020-11-21 11:42:26 +0100

    initial commit

---
+++ v2/number_gen.py
@@ -0,0 +1,17 @@
+import random
+
+
+def number_generator(n):
+    arr = []
+    for i in range(1, n+1):
+        arr.append(i)
+    
+    return arr
+
+try:
+    n = int(input("Enter array size"))
+except Exception:
+    print("Input wasn't a number")
+    
+arr = number_generator(n)
+print(arr)
