9a8a2cc6cf2222801cc9c7711897c65339feb9f0
Milena Vujicic <vujicic_milena@uns.ac.rs>
2020-11-21 11:53:15 +0100

    added primes

--- v2/number_gen.py
+++ v2/number_gen.py
@@ -1,3 +1,5 @@
+import math
+
 def number_generator(n):
     arr = []
     for i in range(1, n+1):
@@ -5,10 +7,26 @@ def number_generator(n):
     
     return arr
 
+def prime_number(arr):
+    length = len(arr)
+    flag = True
+    ret_arr = []
+
+    for a in range(2, length+1):
+        for b in range(2, math.floor(math.ceil(a))):
+            if a%b == 0:
+                flag = False
+        if flag == True:
+            ret_arr.append(a)
+        flag = True        
+                
+    return ret_arr
+
 try:
-    n = int(input("Enter array size"))
+    n = int(input("Enter array size "))
 except Exception:
     print("Input wasn't a number")
     
 arr = number_generator(n)
-print(arr)
+print("The array is ", arr)
+print("The primes are ", prime_number(arr))
